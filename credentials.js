8711a2a2aed09f2013dbe2f34ac0c3afa27b4dff
Akash Kumar <89804806+akashqumar@users.noreply.github.com>
2023-12-07 21:29:59 +0530

    Auto Reply Gmail Bot

---
+++ credentials.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = "694389462621-e0akqcviromam83slv3m0rcr352rr0mb.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//04DOcMMFWMh1uCgYIARAAGAQSNwF-L9IrLGbxNfcnpORVtbX1ckuU-foBiTPxVG4J8wuiY4T7yu7YL4Zx5vJ8lAg4zOCMmes_X2M"
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
